0766a7fc3a71dabc96d12dfab61e7bf5ceb54b8c
xin <515361725@qq.com>
2020-10-28 11:50:13 +0800

    小视频去水印工具

---
+++ src/main/java/com/xiaofu/douyin/WatermarkKillerApplication.java
@@ -0,0 +1,13 @@
+package com.xiaofu.douyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermarkKillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermarkKillerApplication.class, args);
+	}
+
+}
